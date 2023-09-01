c6963bf069531923160ee508e032c8d6f7821c10
abula28 <nikaabuladze28@gmail.com>
2023-09-01 11:38:00 +0400

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+const symbols = [
+  {
+    characters: "abscdefghijklmnopqrstuvwxyz",
+    numbers: "1234567890",
+    symbols: `!@#$%^&*()_-=+'"|/.,?`,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      symbols,
+    },
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(
+    `App listening on port ${port}, checkout http://localhost:${port}`
+  );
+});
